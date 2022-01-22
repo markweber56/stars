714ecf451d5a477efeba1852d81c00d9d050cd90
Weber <Mark.Weber@ehi.com>
2022-01-22 11:52:00 -0600

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { FaStar } from "react-icons/fa";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+		  <body className="App-body">
+			<StarRating />
+		  </body>
       </header>
     </div>
   );
+}
+
+function StarRating() {
+	return [
+		<FaStar color="red" />,
+		<FaStar color="red" />,
+		<FaStar color="red" />,
+		<FaStar color="grey" />,
+		<FaStar color="grey" />
+	];
 }
 
 export default App;
