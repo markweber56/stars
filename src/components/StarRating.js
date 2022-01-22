670f7e6f47f3ea9c80f418204f06ee9066fea8ad
Weber <Mark.Weber@ehi.com>
2022-01-22 12:04:30 -0600

    make componenets directory

---
+++ src/components/StarRating.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function StarRating() {
+	return [
+		<FaStar color="red" />,
+		<FaStar color="red" />,
+		<FaStar color="red" />,
+		<FaStar color="grey" />,
+		<FaStar color="grey" />
+	];
+}
