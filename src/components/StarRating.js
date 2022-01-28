e4f6eaac3a16a6bbf4288b14fcf7afc2d2bf1f90
Weber <Mark.Weber@ehi.com>
2022-01-28 16:00:22 -0600

    working

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaStar } from "react-icons/fa";
+import { createArray } from "../util/Utils";
 
-export default function StarRating() {
-	return [
-		<FaStar color="red" />,
-		<FaStar color="red" />,
-		<FaStar color="red" />,
-		<FaStar color="grey" />,
-		<FaStar color="grey" />
-	];
-}
+// default function for onSelect does nothing
+const Star = ({ selected = false, onSelect = f => f }) => (
+	<FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
+);
+
+export default function starRating({ numStars = 5 }) {
+	// value sent to useState is default value for the state variable
+	const [selectedStars, setSelectedStars] = useState(0);
+	return (
+		<>
+			{createArray(numStars).map((n, i) => (
+				<Star 
+					key={i} 
+					selected={selectedStars > i} 
+					onSelect={() => setSelectedStars(i + 1)}
+				/>
+			))}
+			<p>
+				{selectedStars} of {numStars} stars
+			</p>
+		</>
+	);
+};
+// return createArray(numStars).map((n, i) => <Star key={i} />);
